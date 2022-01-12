ce03fa4d093b07376a09a36c11aa3b358d223a74
Lazzzer <lazarpavicevic@protonmail.com>
2022-01-12 23:34:08 +0100

    chore: remove useless files

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,27 @@
 <template>
-<div class="bg-blue-400">
-  <Navbar />
-  <router-view />
-</div>
+  <div class="bg-blue-400">
+    <Navbar />
+    <router-view />
+  </div>
 </template>
 
 <script>
-import { defineComponent, computed, reactive } from 'vue'
+import { defineComponent } from 'vue'
 import { useHead } from '@vueuse/head'
 import Navbar from './components/global/Navbar.vue'
 
 export default defineComponent({
-    setup() {
-        const siteData = reactive({
-            title: `Bibliotkt`,
-            description: `Beatiful library web application`,
-        });
-        useHead({
-            // Can be static or computed
-            title: computed(() => siteData.title),
-            meta: [
-                {
-                    name: `description`,
-                    content: computed(() => siteData.description),
-                },
-            ],
-            link: [
-                {
-                    rel: "stylesheet",
-                    href: "https://rsms.me/inter/inter.css"
-                }
-            ]
-        });
-    },
-    components: { Navbar }
+  setup() {
+    useHead({
+      title: "Bibliotkt",
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://rsms.me/inter/inter.css"
+        }
+      ]
+    });
+  },
+  components: { Navbar }
 })
 </script>
