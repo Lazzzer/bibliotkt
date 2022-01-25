6bfcf4a23246c01aa023a5040f03600c885b0ed1
Lazzzer <lazarpavicevic@protonmail.com>
2022-01-25 15:41:34 +0100

    feat: Crud categorie ok

---
+++ server/Controllers/CategorieController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using server.Models;
+using server.Services;
+
+namespace server.Controllers;
+
+[ApiController]
+public class CategorieController : ControllerBase
+{
+    private readonly ICategorieService _service;
+
+    public CategorieController(ICategorieService service)
+    {
+        _service = service;
+    }
+    
+    [Route("categories")]
+    [HttpGet]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public ActionResult GetCategories()
+    {
+        var list = _service.GetCategories();
+        if (list.Count == 0)
+            return NotFound();
+
+        var stringList = list.Select(c => c.Nom);
+        
+        return Ok(stringList);
+    }
+    
+    [Route("categorie/{nom}")]
+    [HttpGet]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public ActionResult GetCategorieByNom(string nom)
+    {
+        var categorie = _service.GetCategorieByNom(nom);
+        if (categorie == null)
+            return NotFound();
+
+        return Ok(categorie);
+    }
+    
+    [Route("categorie")]
+    [HttpPost]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public ActionResult CreateCategorie(string nom)
+    {
+        var fetchedCategorie = _service.GetCategorieByNom(nom);
+        
+        if (fetchedCategorie == null)
+            return Created("Created", new { Issn = _service.Insert(nom) });
+
+        return BadRequest("Categorie already exists");
+    }
+
+    [Route("categorie")]
+    [HttpPut]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public ActionResult UpdateCategorie(string nom, string newNom)
+    {
+        var fetchedCategorie = _service.GetCategorieByNom(nom);
+        if (fetchedCategorie == null)
+            return NotFound();
+            
+        return Accepted("Updated", new { AffectedRow = _service.Update(nom, newNom) });
+        
+    }
+    
+    [Route("categorie")]
+    [HttpDelete]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public ActionResult DeleteCategorie(string nom)
+    {
+        var fetchedCategorie = _service.GetCategorieByNom(nom);
+        if (fetchedCategorie == null)
+            return NotFound();
+
+        return Accepted("Deleted", new { AffectedRow = _service.Delete(nom) });
+    }
+    
+}
