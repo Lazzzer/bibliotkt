6edb0d6a6dcf168c644aaeef76e14058c425b1da
Lazzzer <lazarpavicevic@protonmail.com>
2022-01-26 14:55:09 +0100

    refactor

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,22 @@
-<script setup>
-import { useHead } from '@vueuse/head'
-import Navbar from './components/global/Navbar.vue'
-    useHead({
-      title: "Bibliotkt",
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://rsms.me/inter/inter.css"
-        }
-      ]
-    });
-</script>
-
 <template>
-  <div class="bg-blue-400">
+  <div>
     <Navbar />
     <router-view />
   </div>
 </template>
+
+<script setup>
+import { useHead } from '@vueuse/head'
+import Navbar from './components/global/Navbar.vue'
+
+useHead({
+  title: "Bibliotkt",
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://rsms.me/inter/inter.css"
+    }
+  ]
+});
+
+</script>
