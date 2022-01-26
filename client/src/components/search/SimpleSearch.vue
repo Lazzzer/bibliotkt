ef3139a68c1da38e8c1396fb455c6dbc73063f66
Lazzzer <lazarpavicevic@protonmail.com>
2022-01-26 17:11:37 +0100

    refactor: add no result in simpleSearch

--- client/src/components/search/SimpleSearch.vue
+++ client/src/components/search/SimpleSearch.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div>
-    <label for="email" class="block text-sm font-medium text-gray-700">Titre du livre</label>
+    <label for="email" class="block text-lg font-bold text-sky-700">Titre du livre</label>
     <div class="mt-1">
       <input
         v-model="title"
@@ -14,7 +14,8 @@
         class="p-2 my-4 text-sm text-white rounded-md bg-sky-700"
       >Appliquer</button>
 
-      <BookGrid v-if="livres.length > 0" :livres="livres" />
+      <BookGrid v-if="livres.length > 0 && !fetchErr" :livres="livres" />
+      <div v-if="fetchErr" class="italic"> Pas de résultats</div>
     </div>
   </div>
 </template>
@@ -26,15 +27,21 @@ import { ref } from "vue";
 import BookGrid from "../global/BookGrid.vue";
 
 const title = ref("");
+const fetchErr = ref(false);
 let livres = ref([]);
 
 
 const fetchLivresByTitle = async () => {
   axios.get('livresByTitle', { params: { titre: title.value } }).then(res => {
     livres.value = res.data;
+    fetchErr.value = false;
     console.log(livres)
   }).catch(err => {
     console.log(err.response.data);
+    if (err.response.data.status === 404 || err.response.data.status === 400) {
+      console.log(err.response.data.status)
+      fetchErr.value = true;
+    } 
   })
 }
 
